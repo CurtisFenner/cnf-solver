816b7e0dc5cd625727e12c4631e2243f94dd6b0b
Curtis Fenner <cwfenner@umich.edu>
2018-07-22 19:45:32 -0400

    Print CNF before satisfaction result

--- run_dimacs.lua
+++ run_dimacs.lua
@@ -5,6 +5,24 @@ table.unpack = table.unpack or unpack
 
 local source = io.read "*all"
 local cnf = dimacs(source)
+
+if true then
+	for _, clause in ipairs(cnf:clauseList()) do
+		io.write("& (")
+		for i, literal in ipairs(clause) do
+			if i ~= 1 then
+				io.write(" | ")
+			end
+
+			if literal[2] == false then
+				io.write("~")
+			end
+			io.write(literal[1])
+		end
+		print(")")
+	end
+end
+
 local log = {}
 local sat = cnf:isSatisfiable(log)
 print("SAT:", not not sat)
